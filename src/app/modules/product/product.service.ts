de4543bec2e51b2f960381ef1d8a9350bebeb4f0
Shonsu <shonsu82@gmail.com>
2022-11-11 00:06:47 +0100

    getting data form backend

--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Product } from './model/Product';
 
 @Injectable({
@@ -6,31 +8,9 @@ import { Product } from './model/Product';
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  getProducts(): Product[] {
-    return [
-      {
-        name: "Product 1",
-        category: "Kategoria 1",
-        description: "Opis produktu 1",
-        price: 11.99,
-        currency: "PLN"
-      },
-      {
-        name: "Product 2",
-        category: "Kategoria 2",
-        description: "Opis produktu 2",
-        price: 23.99,
-        currency: "PLN"
-      },
-      {
-        name: "Product 3",
-        category: "Kategoria 3",
-        description: "Opis produktu 3",
-        price: 31.99,
-        currency: "PLN"
-      }
-    ];
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>("/api/products");
   }
 }
