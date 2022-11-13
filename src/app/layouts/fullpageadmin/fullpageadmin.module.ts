8da5122b35bc1efaf3d8c9f3015292e076aced8b
Shonsu <shonsu82@gmail.com>
2022-11-13 23:39:01 +0100

    added admin product view

--- src/app/layouts/fullpageadmin/fullpageadmin.module.ts
+++ src/app/layouts/fullpageadmin/fullpageadmin.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { AdminProductComponent } from 'src/app/modules/admin/admin-product/admin-product.component';
 import { AdminComponent } from 'src/app/modules/admin/admin.component';
 import { MaterialModule } from 'src/app/shared/material.module';
 import { FullpageadminComponent } from './fullpageadmin.component';
@@ -9,7 +10,8 @@ import { FullpageadminComponent } from './fullpageadmin.component';
 @NgModule({
   declarations: [
     FullpageadminComponent,
-    AdminComponent
+    AdminComponent,
+    AdminProductComponent
   ],
   imports: [
     CommonModule,
