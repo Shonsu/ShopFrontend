1a46259f996c98d43f0da77df56adfee9933bb27
Shonsu <shonsu82@gmail.com>
2022-11-27 19:24:33 +0100

    added category service for admin panel

--- src/app/modules/admin/admin-product-form/admin-product-form.component.ts
+++ src/app/modules/admin/admin-product-form/admin-product-form.component.ts
@@ -2,6 +2,8 @@
 import { Component, Input, OnInit } from "@angular/core";
 import { GridAreaStyleBuilder } from "@angular/flex-layout";
 import { FormGroup } from "@angular/forms";
+import { AdminCategoryNameDto } from "./adminCategoryNameDto";
+import { FormCategoryService } from "./form-category.service";
 
 @Component({
     selector: 'app-admin-product-form',
@@ -53,13 +55,14 @@ import { FormGroup } from "@angular/forms";
 
         <mat-form-field appearance="fill">
             <mat-label>Kategoria</mat-label>
-            <input matInput placeholder="Podaj kategorię produktu" formControlName="category">
-            <div *ngIf="category?.invalid && (category?.dirty || category?.touched\)" class="errorMessages">
-                <div *ngIf="category?.errors?.['required']">
+            <mat-select formControlName="categoryId">
+                <mat-option *ngFor="let el of categories" [value]="el.id">
+                {{el.name}}
+                </mat-option>
+            </mat-select>
+            <div *ngIf="categoryId?.invalid && (categoryId?.dirty || categoryId?.touched\)" class="errorMessages">
+                <div *ngIf="categoryId?.errors?.['required']">
                     Kategoria jest wymagana
-                </div>
-                <div *ngIf="category?.errors?.['minlength']">
-                    Kategoria musi mieć przynajmniej 4 znaki
                 </div>
             </div>
         </mat-form-field>
@@ -75,6 +78,7 @@ import { FormGroup } from "@angular/forms";
                     Cena musi być większa od zera
                 </div>
             </div>
+            
         </mat-form-field>
 
         <mat-form-field appearance="fill">
@@ -91,44 +95,56 @@ import { FormGroup } from "@angular/forms";
             <button mat-flat-button color="primary" [disabled]="!parentForm.valid">Zapisz</button>
         </div>
     </div>`,
-        styles: [`
+    styles: [`
         .errorMessages{
             color: red;
             font-style: italic;
             padding-top: 5px;
         }
         `]
-    
+
 })
 export class AdminProductFormComponent implements OnInit {
 
     @Input() parentForm!: FormGroup;
+    categories: Array<AdminCategoryNameDto> = [];
+
+    constructor(private formCategoryService: FormCategoryService) { }
 
     ngOnInit(): void {
+        this.getCategories();
     }
+
+    getCategories() {
+        this.formCategoryService.getCategories()
+            .subscribe(categories => this.categories = categories);
+    }
+
     get name() {
         return this.parentForm.get("name");
     }
 
     get description() {
         return this.parentForm.get("description");
-    } 
-    
-    get category() {
-        return this.parentForm.get("category");
     }
-    
+
+    get categoryId() {
+        return this.parentForm.get("categoryId");
+    }
+
     get price() {
         return this.parentForm.get("price");
     }
-    
+
     get currency() {
         return this.parentForm.get("currency");
     }
+
     get slug() {
         return this.parentForm.get("slug");
     }
-        get fullDescription() {
+
+    get fullDescription() {
         return this.parentForm.get("fullDescription");
     }
 }
